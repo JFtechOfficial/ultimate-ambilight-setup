6c6c8e0bc9a2e1d18c17f4b92b08f3e1b6cf2be1
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-02-28 13:03:12 +0100

    Added profiles and polished comment

--- scripts/fan.py
+++ scripts/fan.py
@@ -4,8 +4,26 @@ import RPi.GPIO as GPIO
 
 """ Define some variables """
 pin = 26  # pin ID, edit here to change it
+
+# Uncomment only one profile to use it. Edit values as you like
+""" Conservative profile """
 max_TEMP = 70  # Temperature in Celsius after which the fan triggers
 cutoff_TEMP = 60  # Temerature in Celsius after which the fan stops
+sleepTime = 60  # Temperature reading interval in seconds
+
+""" Aggressive profile """
+"""
+max_TEMP = 60  # Temperature in Celsius after which the fan triggers
+cutoff_TEMP = 45  # Temerature in Celsius after which the fan stops
+sleepTime = 1  # Temperature reading interval in seconds
+"""
+
+""" Always ON profile """
+"""
+max_TEMP = 10  # Temperature in Celsius after which the fan triggers
+cutoff_TEMP = 1  # Temerature in Celsius after which the fan stops
+sleepTime = 100  # Temperature reading interval in seconds
+"""
 
 
 def setup():
@@ -17,7 +35,7 @@ def setup():
 
 
 def getCPUtemperature():
-    """ Get CPU temperature """
+    """ Measure CPU temperature """
     res = os.popen('vcgencmd measure_temp').readline()
     temp = (res.replace("temp=", "").replace("'C\n", ""))
     print("temp is {0}".format(temp))  # Uncomment here for testing
@@ -31,10 +49,10 @@ try:
         CPU_temp = float(getCPUtemperature())
         if (CPU_temp > max_TEMP):
             GPIO.output(pin, True)
-            print ("ON")
+            # print ("ON")  # Uncomment here for testing
         if (CPU_temp < cutoff_TEMP):
             GPIO.output(pin, False)
-            print ("OFF")
-        sleep(60)  # Read temperature every 60 sec, edit here to change it
+            # print ("OFF")  # Uncomment here for testing
+        sleep(sleepTime)
 except KeyboardInterrupt:
     GPIO.cleanup()  # clean up GPIO on CTRL+C exit
