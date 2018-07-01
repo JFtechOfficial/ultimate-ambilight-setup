0462209f01fa1d14af37bfeaea56477322649e9b
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-07-01 19:29:47 +0200

    added modular installation

--- install.sh
+++ install.sh
@@ -1,62 +1,69 @@
 #!/bin/bash
 
-    # we want to be root to install
+# we want to be root to install
 if [ $(id -u) != 0 ]; then
-		echo '---> Critical Error: Please run the script as root (sudo sh ./install.sh) -> abort' 
-		exit 1
+  echo '---> Critical Error: Please run the script as root (sudo sh ./install.sh) -> abort'
+  exit 1
 fi
-
+#Check, if year equals 1970
+DATE=$(date +"%Y")
+if [ "$DATE" -le "2015" ]; then
+  echo "---> Critical Error: Please update your systemtime (Year of your system: ${DATE}) -> abort"
+  exit 1
+fi
 
 echo "   ______________            __   "
 echo "  /___    ____/ /____  _____/ /_  "
 echo " __  / / /_  / __/ _ \/ ___/ __ \ "
 echo "/ /_/ / __/ / /_/  __/ /__/ / / / "
 echo "\____/_/    \__/\___/\___/_/ /_/  "
-
+echo ""
 echo "Starting..."
+
+
+interactive=
+fan=0
+buttons=0
+clock=0
+assistant=0
+startup=0
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -f | --fan )            fan=1
+      ;;
+    -b | --buttons )        buttons=1
+      ;;
+    -c | --clock )          clock=1
+      ;;
+    -a | --assistant )      assistant=1
+      ;;
+    -v | --verbose )        verbose=1
+      ;;
+    -i | --interactive )    interactive=1
+      ;;
+    -h | --help )           usage
+      exit
+      ;;
+    * )                     usage
+      exit 1
+  esac
+  shift
+done
+startup=$((fan+buttons+assistant))
+default_install=$((startup+clock))
+if [ $default_install -eq 0 ]; then
+  fan=1
+  buttons=1
+  clock=1
+  assistant=1
+fi
 # Find out if we are on OpenElec (Rasplex) / OSMC / Raspbian
 OS_OPENELEC=`grep -m1 -c 'OpenELEC\|RasPlex\|LibreELEC\|OpenPHT\|PlexMediaPlayer' /etc/issue`
 OS_LIBREELEC=`grep -m1 -c LibreELEC /etc/issue`
 OS_RASPLEX=`grep -m1 -c RasPlex /etc/issue`
 OS_OSMC=`grep -m1 -c OSMC /etc/issue`
 OS_RASPBIAN=`grep -m1 -c 'Raspbian\|RetroPie' /etc/issue`
-
-    interactive=
-    fan=
-    buttons=
-    clock=
-    assistant=
-    
-    while [ "$1" != "" ]; do
-        case $1 in
-            -f | --fan )            fan=1
-                                    ;;
-            -b | --buttons )        buttons=1
-                                    ;;
-            -c | --clock )          clock=1
-                                    ;;
-            -a | --assistant )      assistant=1
-                                    ;;
-            -v | --verbose )        verbose=1
-                                    ;;
-            -i | --interactive )    interactive=1
-                                    ;;
-            -h | --help )           usage
-                                    exit
-                                    ;;
-            * )                     usage
-                                exit 1
-    esac
-    shift
-done
-echo $fan
-
-#Check, if year equals 1970
-DATE=$(date +"%Y")
-if [ "$DATE" -le "2015" ]; then
-        echo "---> Critical Error: Please update your systemtime (Year of your system: ${DATE}) -> abort"
-        exit 1
-fi
 
 # check which init script we should use
 USE_SYSTEMD=`grep -m1 -c systemd /proc/1/comm`
@@ -66,106 +73,97 @@ USE_SERVICE=`which /usr/sbin/service | wc -l`
 #Install dependencies for Hyperion and setup preperation
 if [ $OS_OPENELEC -ne 1 ]; then
 
-    echo -n "Updating System..."
-    sudo apt-get update -y
-    ##sudo apt-get upgrade -y
-    ##sudo apt-get dist-upgrade -y
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
-    echo '---> Stop Hyperion, if necessary'
-if [ $OS_OPENELEC -eq 1 ]; then
+  echo -n "Updating System..."
+  sudo apt-get update -y
+  ##sudo apt-get upgrade -y
+  ##sudo apt-get dist-upgrade -y
+  sudo apt-get autoremove -y
+  sudo apt-get autoclean -y
+  echo '---> Stop Hyperion, if necessary'
+  if [ $OS_OPENELEC -eq 1 ]; then
     killall hyperiond 2>/dev/null
-elif [ $USE_SYSTEMD -eq 1 ]; then
-	service hyperion stop 2>/dev/null
-	#many people installed with the official script and this just uses service, if both registered -> dead
-	/usr/sbin/service hyperion stop 2>/dev/null
-elif [ $USE_INITCTL -eq 1 ]; then
-	/sbin/initctl stop hyperion 2>/dev/null
-elif [ $USE_SERVICE -eq 1 ]; then
-	/usr/sbin/service hyperion stop 2>/dev/null
+  elif [ $USE_SYSTEMD -eq 1 ]; then
+    service hyperion stop 2>/dev/null
+    #many people installed with the official script and this just uses service, if both registered -> dead
+    /usr/sbin/service hyperion stop 2>/dev/null
+  elif [ $USE_INITCTL -eq 1 ]; then
+    /sbin/initctl stop hyperion 2>/dev/null
+  elif [ $USE_SERVICE -eq 1 ]; then
+    /usr/sbin/service hyperion stop 2>/dev/null
+  fi
 fi
+
+
+echo -n "Downloading and installing Python..."
+sudo apt-get install -y build-essential
+sudo apt-get install -y python
+sudo apt-get install -y python-dev
+sudo apt install -y python-pip
+##curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+##python get-pip.py
+
+echo -n "Downloading..."
+wget https://pypi.python.org/packages/source/R/RPi.GPIO/RPi.GPIO-0.6.2.tar.gz
+tar -xf RPi.GPIO-0.6.2.tar.gz --strip-components 1
+sudo python setup.py install
+##sudo rm -rf RPi.GPIO-0.*
+if [ $clock -ne 0 ]; then
+  # Clock effect for Hyperion
+  sudo pip install pyowm
+  sudo mv Hyperion\ effects/clock.py /usr/share/hyperion/effects/
+  sudo mv Hyperion\ effects/clock.json /usr/share/hyperion/effects/
+fi
+if [ $startup -ne 0 ]; then
+  sudo apt install -y curl
+  sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  sudo apt-get install -y npm
+  sudo npm install -g forever
+  sudo npm install -g forever-service
+fi
+if [ $assistant -ne 0 ]; then
+  sudo npm install -g hyperion-client
+  sudo -H pip install --upgrade youtube-dl
+  sudo npm install -g playonkodi
+  sudo forever-service install assistant-service --script scripts/client.js
+  sudo service assistant-service start
 fi
 
-
-    echo -n "Downloading, installing Python..."
-    sudo apt-get install -y build-essential
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR"
-        exit 1
-    fi
-    sudo apt-get install -y python
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR"
-        exit 1
-    fi
-    sudo apt-get install -y python-dev
-    sudo apt install -y python-pip
-    ##curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    ##python get-pip.py
-
-    echo -n "Downloading..."
-    wget https://pypi.python.org/packages/source/R/RPi.GPIO/RPi.GPIO-0.6.2.tar.gz
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR"
-        exit 1
-    fi
-    tar -xf RPi.GPIO-0.6.2.tar.gz --strip-components 1
-    sudo python setup.py install
-    ##sudo rm -rf RPi.GPIO-0.*
-    # Clock effect for Hyperion
-    sudo pip install pyowm
-    if [ $? -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR"
-        exit 1
-    fi
-    sudo mv Hyperion\ effects/clock.py /usr/share/hyperion/effects/
-    sudo mv Hyperion\ effects/clock.json /usr/share/hyperion/effects/
-    sudo apt install -y curl
-    sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-    sudo apt-get install -y npm
-    sudo npm install -g forever
-    sudo npm install -g forever-service
-    sudo npm install -g hyperion-client
-    sudo -H pip install --upgrade youtube-dl
-    sudo npm install -g playonkodi
-    sudo forever-service install assistant-service --script scripts/client.js 
-    sudo forever-service install fan-service -s scripts/fan.py -f " -c python"
-    sudo forever-service install buttons-service -s scripts/buttons.py -f " -c python"
-    ##sudo apt-get install cron -y
-    ##if [ $? -eq 0 ]; then
-    ##    echo "OK"
-    ##else
-    ##    echo "ERROR"
-    ##    exit 1
-    ##fi
+if [ $fan -ne 0 ]; then
+  sudo forever-service install fan-service -s scripts/fan.py -f " -c python"
+  sudo service fan-service start
+fi
+if [ $buttons -ne 0 ]; then
+  sudo forever-service install buttons-service -s scripts/buttons.py -f " -c python"
+  sudo service buttons-service start
+fi
+##sudo apt-get install cron -y
+##if [ $? -eq 0 ]; then
+##    echo "OK"
+##else
+##    echo "ERROR"
+##    exit 1
+##fi
 
 
 echo '---> Starting Hyperion'
 if [ $OS_OPENELEC -eq 1 ]; then
-	/storage/.config/autostart.sh > /dev/null 2>&1 &
+  /storage/.config/autostart.sh > /dev/null 2>&1 &
 elif [ $USE_SYSTEMD -eq 1 ]; then
-	systemctl start hyperion
+  systemctl start hyperion
 elif [ $USE_INITCTL -eq 1 ]; then
-	/sbin/initctl start hyperion
+  /sbin/initctl start hyperion
 elif [ $USE_SERVICE -eq 1 ]; then
-	/usr/sbin/service hyperion start
+  /usr/sbin/service hyperion start
 fi
+# cleanup -> TO-DO
 echo
 echo -n "REBOOT NOW? [y/N]"
 read
 if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then
-    echo "Reboot started..."
-    reboot
+  echo "Reboot started..."
+  reboot
 else
-    echo
-    echo "Done"
+  echo
+  echo "Done"
 fi
