95e2173ec66e6a9d434044b333776d258628cb8f
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-07-10 17:13:02 +0200

    Update jsonHelper.py

--- jsonHelper.py
+++ jsonHelper.py
@@ -1,19 +1,17 @@
 import json
 import sys
 
-print(sys.argv)
 with open(sys.argv[1], 'r+') as f:
-     data = json.load(f)
-     if (sys.argv[2] == '0'){
-         data['args'][sys.argv[3]] = int(sys.argv[4])
-         }
-     if (sys.argv[2] == '1'){
-         data['args'][sys.argv[3]][sys.argv[4]] = int(sys.argv[5])
-         }
-     if (sys.argv[2] == '2'){
-         data['args'][sys.argv[3]][sys.argv[4]][sys.argv[5]] = int(sys.argv[6])
-         }
-     f.seek(0)
-     f.truncate()
-     json.dump(data, f)
-
+    data = json.load(f)
+    if (len(sys.argv) == 4):
+        data['args'][sys.argv[2]] = sys.argv[3]
+        
+    if (len(sys.argv) == 5):
+        data['args'][sys.argv[2]][sys.argv[3]] = sys.argv[4]
+        
+    if (len(sys.argv) == 6):
+        data['args'][sys.argv[2]][sys.argv[3]][sys.argv[4]] = sys.argv[5]
+    
+    f.seek(0)
+    f.truncate()
+    json.dump(data, f, indent=4)
