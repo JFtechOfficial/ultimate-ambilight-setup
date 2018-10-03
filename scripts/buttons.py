510a06c938ee363932e3bc2672cb5f2cc2618aa8
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-10-03 16:43:41 +0200

    Update buttons.py

--- scripts/buttons.py
+++ scripts/buttons.py
@@ -1,4 +1,4 @@
-# import json
+#import json
 import commentjson
 import os
 import subprocess
@@ -7,15 +7,14 @@ import RPi.GPIO as GPIO
 
 """
 Define some variables editing the button.json config file
-(please do NOT use pin 5 (BOARD): it must be used as shutdown button)
+please do NOT use pin 5 (BOARD): it must be used as power button
 """
 with open('buttons.json') as f:
     data = commentjson.load(f)
-    clear = data['args']['clear']  # Pin ID
-    effects = data['args']['effects']
-    pins = {}  # {Pin ID: 'Effect name'}
-    for name in effects:
-        pins.update({effects[name]: name})
+gpio_setup = data['args']['gpio-setup']
+priority = data['args']['priority']
+if not priority:
+    priority = 100
 
 FNULL = open(os.devnull, 'w')
 
@@ -26,53 +25,85 @@ def launch(*args):
         subprocess.call(['hyperion-remote', '--clearall'], shell=False, stdout=FNULL, stderr=subprocess.STDOUT)
     if len(args) == 3:
         subprocess.call(['hyperion-remote', str(args[0]), str(args[1]), '--priority', str(args[2])], shell=False, stdout=FNULL, stderr=subprocess.STDOUT)
-
-
-def Effect(channel):
+    
+    
+def effect(name, priority):
     """Start an effect."""
-    launch('-e', pins[channel])
+    launch('-e', name, priority)
 
 
-def Clear(channel):
-    """Clear all effects (short press) or set color to black (long press)."""
+def color(rgbColor, priority):
+    """Start a color."""
+    hexColor = '%02x%02x%02x' % tuple(rgbColor)
+    launch('--color', hexColor, priority)
+
+
+def clear_all():
+    """Clear all effects/colors."""
+    launch('--clearall')
+
+
+def button_press(channel, duration):
+    """Route button press."""
+    pin = str(channel)
+    on_press = gpio_setup[pin][duration]
+    if isinstance(on_press, list):
+        print("COLOR")
+        if duration == 'long-press' and gpio_setup[pin]['short-press'] == 'clear':
+            print("lower")
+            with open('/etc/hyperion/hyperion.config.json') as config:
+                hyperion_data = commentjson.load(config)
+            color(on_press, hyperion_data['grabber-v4l2']['priority'] + 1)
+        color(on_press, priority)
+    elif isinstance(on_press, str):
+        if on_press == 'clear':
+            print("CLEAR")
+            clear_all()
+        else:
+            print("EFFECT")
+            effect(on_press, priority)
+
+
+def is_long_press(channel):
+    """Detect long button press or short button press."""
     start = time.time()
     stop = start
+    time.sleep(0.02)
     while GPIO.input(channel) == GPIO.LOW:
         time.sleep(0.01)
         stop = time.time() - start
         if stop > 1:
-            with open('/etc/hyperion/hyperion.config.json') as config:
-                data = commentjson.load(config)
-            priority = data['grabber-v4l2']['priority']
-            # print(priority)
-            launch('--clearall')
-            launch('--color', '000000', priority + 1)
-            return
-    print(stop)
-    if stop > 0.02:
-        launch('--clearall')
+            button_press(channel, 'long-press')
+    return button_press(channel, 'short-press')
 
 
 def setup():
     """Raspberry Pi GPIO setup."""
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    for key in pins:
-        GPIO.setup(key, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(key, GPIO.FALLING, callback=Effect, bouncetime=300)
-    GPIO.setup(clear, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(clear, GPIO.FALLING, callback=Clear, bouncetime=300)
-
+    power_button = 3
+    if data['args']['gpio-mode'] == 'BOARD':
+        GPIO.setmode(GPIO.BOARD)
+        power_button = 5
+    print(power_button)
+    GPIO.setup(power_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    for pin in gpio_setup:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        pin_setup = gpio_setup[pin]
+        if pin_setup['short-press'] or pin_setup['long-press']:
+            print(pin)
+            GPIO.add_event_detect(int(pin), GPIO.FALLING, callback=islongpress, bouncetime=300)
+    
 
 setup()
 """ Waiting for shutdown button """
 try:
-    GPIO.wait_for_edge(3, GPIO.FALLING)
+    GPIO.wait_for_edge(power_button, GPIO.FALLING)
     launch('--clearall')
     GPIO.cleanup()
     time.sleep(1)
     subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()  # clean up GPIO on CTRL+C exit
+    pass
+    GPIO.cleanup()
