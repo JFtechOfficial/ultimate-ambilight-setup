d2ff72bde70fb71dc33fbf64ae9e0719ad8df09d
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-10-18 19:32:10 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -149,12 +149,12 @@ fi
 if [ $assistant -ne 0 ]; then
   echo -n "Douwnloading Google Assisant client script..."
   git clone https://github.com/JFtechOfficial/hyperion-mqtt-subscriber.git
-  rename -f hyperion-mqtt-subscriber Google_Assistant
+  mv -f hyperion-mqtt-subscriber Google_Assistant
 fi
 if [ $fan -ne 0 ]; then
   echo -n "Downloading fan script..."
   git clone https://github.com/JFtechOfficial/Raspberry-Pi-PWM-fan.git
-  rename -f Raspberry-Pi-PWM-fan fan
+  mv -f Raspberry-Pi-PWM-fan fan
 fi
 if [ $buttons -ne 1 ]; then
   sudo rm -rf buttons/
